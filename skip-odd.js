d57d1766132ab67bec8e0d4d7c5843dc9fd2dafd
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-03 17:34:31 +0600

    added skip operation

---
+++ skip-odd.js
@@ -0,0 +1,20 @@
+let n=1;
+while(n<=40)
+{
+    n++;
+    if(n%2==1)
+    {
+        continue;
+    }
+    console.log(n);
+    
+}
+// ******for******
+for(let n=1;n<=40;n++)
+{
+    if(n%2!==0)
+    {
+        continue;
+    }
+    console.log(n);
+}

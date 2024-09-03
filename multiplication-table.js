ad476f77a26bb3cc5575fc9ee1779ff44f28156c
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-03 16:14:20 +0600

    added multiplication-table by while loop

---
+++ multiplication-table.js
@@ -0,0 +1,16 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+let num=1;
+let table=5;
+while(num<=10)
+{
+   
+   
+   console.log(table+'*'+num+'='+table*num);
+   num++;
+   
+   
+}

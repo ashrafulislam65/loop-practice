eef5bda0ed2413828223b3ea91a42364bc80da24
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-03 16:31:55 +0600

    added even-odd by for loop

---
+++ even-odd-for.js
@@ -0,0 +1,32 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+// ****odd***
+for(let num=61;num<=100;num++)
+{
+    if(num%2!==0)
+    {
+        console.log(num);
+    }
+}
+// ****even***
+for(let num1=78;num1<=98;num1++)
+    {
+        if(num1%2==0)
+        {
+            console.log(num1);
+        }
+    }

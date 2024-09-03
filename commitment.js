57eeb153ca0c5e6dd41d3215e13bcf36adf73819
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-03 14:18:11 +0600

    added commitmet js

---
+++ commitment.js
@@ -0,0 +1,11 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+var days = 0;
+while(days<=60)
+{
+    console.log('I Will invest at least 6hrs every days for next 60 days!')
+    days++;
+}

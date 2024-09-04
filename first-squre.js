303f6901eadcf58dd9f8c4c3444b45330a0ecd25
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-04 13:47:02 +0600

    added find first square num and break

---
+++ first-squre.js
@@ -0,0 +1,18 @@
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+let num=1;
+square=0;
+while(num<=100)
+{ 
+    
+   if(num==2*2)
+    {
+        break;
+    }
+    console.log(num);
+    num++; 
+   
+    
+    
+}
